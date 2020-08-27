76f15e0d7e8021f5a960d3d49fdfbae2844f748e
Joel Joseph <43024731+JoelJoseph01@users.noreply.github.com>
2020-08-27 11:36:06 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logoutuser'),
     path('changepassword/', views.changepassword, name='changepassword'),
     path('change_done/',views.change_done, name='change_done'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='todo/password_reset.html',success_url = reverse_lazy('password_reset_done')), name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='todo/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='todo/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='todo/password_reset_complete.html'), name='password_reset_complete'),
 
     # Todos
     path('', views.home, name='home'),
